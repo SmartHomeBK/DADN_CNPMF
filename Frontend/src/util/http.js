55aaec7a9fdcb2ddb5dc4e23a509b50c26479aff
mahara0511 <122422522+mahara0511@users.noreply.github.com>
2025-04-13 00:28:44 +0700

    Fix: FE Base Url

--- Frontend/src/util/http.js
+++ Frontend/src/util/http.js
@@ -1,24 +1,24 @@
-import axios from "axios";
-const token = localStorage.getItem("UserToken");
+import axios from 'axios';
+const token = localStorage.getItem('UserToken');
 
-console.log("token in http: ", token);
+console.log('token in http: ', token);
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
-  withCredentials: true,
-  timeout: 30000,
-  headers: {
-    authorization: `Bearer ${token}`,
-  },
+    baseURL: 'https://smart-home-iot-backend.onrender.com/api',
+    withCredentials: true,
+    timeout: 30000,
+    headers: {
+        authorization: `Bearer ${token}`,
+    },
 });
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("UserToken");
-    if (token) {
-      config.headers["authorization"] = `Bearer ${token}`;
+    (config) => {
+        const token = localStorage.getItem('UserToken');
+        if (token) {
+            config.headers['authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
