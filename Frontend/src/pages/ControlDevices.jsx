c64aea78736ff33c020874d13a6a081e4695e2f8
VoTranMinhKhoa <167698274+VoTranMinhKhoa@users.noreply.github.com>
2025-04-16 05:01:25 +0700

    Fix UI

--- Frontend/src/pages/ControlDevices.jsx
+++ Frontend/src/pages/ControlDevices.jsx
@@ -64,14 +64,18 @@ const AddDeviceModal = ({ isOpen, onClose, onAdd }) => {
               <label className="block text-sm font-medium text-gray-700 mb-2">
                 Device Name
               </label>
-              <input
-                type="text"
+              <select
                 name="name"
                 value={newDevice.name}
                 onChange={handleChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
-              />
+              >
+                <option value="" disabled>
+                  Select a device
+                </option>
+                {/* Call API Get */}
+              </select>
             </div>
 
             <div className="mb-4">
@@ -85,7 +89,7 @@ const AddDeviceModal = ({ isOpen, onClose, onAdd }) => {
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 <option value="light">Light</option>
-                <option value="fan">Fan</option>
+                <option value="tempurature">Tempurature</option>
               </select>
             </div>
             <div className="mb-4">
