12d394e81bb05d48e49ffbc6768616790b164fe3
VoTranMinhKhoa <167698274+VoTranMinhKhoa@users.noreply.github.com>
2025-04-20 05:36:59 +0700

    Update UI

--- Frontend/src/pages/ControlDevices.jsx
+++ Frontend/src/pages/ControlDevices.jsx
@@ -5,7 +5,7 @@ import { axiosInstance } from "../util/http.js";
 import { Space, Table, Switch, Row, Col, Slider, InputNumber } from "antd";
 import { toast } from "react-hot-toast";
 
-const deviceNames = ["lightBulb", "fan"];
+const deviceNames = ["LightBulb", "Fan"];
 
 const AddDeviceModal = ({ isOpen, onClose, onAdd }) => {
   const [deviceName, setDeviceName] = useState("");
